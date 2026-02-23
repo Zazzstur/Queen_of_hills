b4219a8e2e72633c58ca1742484ea70260c9612a
Zazzstur <zazzstur@gmail.com>
2026-02-23 10:23:15 +0530

    chore: embed env variables in codebase to bypass dashboard configuration requirement

--- .wrangler/tmp/bundle-U0b1Ih/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-U0b1Ih/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\MSI\\Desktop\\website darjeeling\\.wrangler\\tmp\\bundle-CNPFzt\\middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\MSI\\Desktop\\website darjeeling\\.wrangler\\tmp\\bundle-U0b1Ih\\middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "C:\\Users\\MSI\\AppData\\Roaming\\npm\\node_modules\\wrangler\\templates\\middleware\\common.ts";
-import type { WorkerEntrypointConstructor } from "C:\\Users\\MSI\\Desktop\\website darjeeling\\.wrangler\\tmp\\bundle-CNPFzt\\middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "C:\\Users\\MSI\\Desktop\\website darjeeling\\.wrangler\\tmp\\bundle-U0b1Ih\\middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "C:\\Users\\MSI\\Desktop\\website darjeeling\\.wrangler\\tmp\\bundle-CNPFzt\\middleware-insertion-facade.js";
+export * from "C:\\Users\\MSI\\Desktop\\website darjeeling\\.wrangler\\tmp\\bundle-U0b1Ih\\middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
